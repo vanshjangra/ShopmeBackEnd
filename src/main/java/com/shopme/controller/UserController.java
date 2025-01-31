9f2cfb0b2d907b52abde3f4ef8800d9b28f9b848
vanshjangra <vanshjangra586@gmail.com>
2025-01-31 19:13:28 +0530

    code user authorization

--- src/main/java/com/shopme/controller/UserController.java
+++ src/main/java/com/shopme/controller/UserController.java
@@ -63,7 +63,7 @@ public class UserController {
         model.addAttribute("reverseSortDir", reverseSortDir);
         model.addAttribute("keyword", keyword);
 
-        return "users";
+        return "users/users";
     }
 
     @GetMapping("/users/new")
@@ -77,7 +77,7 @@ public class UserController {
         model.addAttribute("listRoles", listRoles);
         model.addAttribute("pageTitle", "Create New User");
 
-        return "user_form";
+        return "users/user_form";
     }
 
     @PostMapping("/users/save")
@@ -118,7 +118,7 @@ public class UserController {
             model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
             model.addAttribute("listRoles", listRoles);
 
-            return "user_form";
+            return "users/user_form";
         } catch (UserNotFoundException ex) {
             redirectAttributes.addFlashAttribute("message", ex.getMessage());
             return "redirect:/users";
--- src/main/java/com/shopme/security/WebSecurityConfig.java
+++ src/main/java/com/shopme/security/WebSecurityConfig.java
@@ -41,7 +41,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests()
+                .antMatchers("/users/**").hasAuthority("Admin")
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
