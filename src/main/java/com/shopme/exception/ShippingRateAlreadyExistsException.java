819c965fab015ae03c76de0a4873f33a7a4f345d
vanshjangra <vanshjangra586@gmail.com>
2025-03-04 20:47:00 +0530

    code shipping rates management module

---
+++ src/main/java/com/shopme/exception/ShippingRateAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.shopme.exception;
+
+public class ShippingRateAlreadyExistsException extends Exception {
+
+    public ShippingRateAlreadyExistsException(String message) {
+        super(message);
+    }
+}
