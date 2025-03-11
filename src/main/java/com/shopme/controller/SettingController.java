4d32315f1faf39e9d3889a3f707eb9a82bd235b1
vanshjangra <vanshjangra586@gmail.com>
2025-03-11 19:19:50 +0530

    code checkout module

--- src/main/java/com/shopme/controller/SettingController.java
+++ src/main/java/com/shopme/controller/SettingController.java
@@ -109,4 +109,14 @@ public class SettingController {
 
         return "redirect:/settings";
     }
+
+    @PostMapping("/settings/save_payment")
+    public String savePaymentSettings(HttpServletRequest request, RedirectAttributes ra){
+        List<Setting> paymentSettings = service.getPaymentSettings();
+        updateSettingValuesFromForm(request, paymentSettings);
+
+        ra.addFlashAttribute("message", "Payment settings has been saved");
+
+        return "redirect:/settings#payment";
+    }
 }
